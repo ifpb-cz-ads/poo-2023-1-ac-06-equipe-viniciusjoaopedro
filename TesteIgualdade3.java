cf20125db015aa1bac7763fedb2ffd8bb88aa4f8
João Pedro <54006291+JoaoPedroli@users.noreply.github.com>
2023-04-19 09:20:01 -0300

    Update TesteIgualdade3.java

--- TesteIgualdade3.java
+++ TesteIgualdade3.java
@@ -1,2 +1,11 @@
-
-class TesteIgualdade3 { public static void main(String args[]){ String frasel: String frase2: frasel = new String("Testando igualdade!! frase2 = new String("Testando igualdade!"). System.out.println( "Frase 1 e Frase 2 possuen o mesmo conteudo? R +(frasel equals(frase?)): System.out.println( Frase 1 "+frasel); System out println("Frase 2 "+frase2)
+public class TesteIgualdade3 {
+    public static void main(String args[]){
+        String frasel;
+        String frase2;
+        frasel = new String("Testando igualdade!");
+        frase2 = new String("Testando igualdade!");
+        System.out.println("Frase 1 " + frasel);
+        System.out.println("Frase 2 " + frase2);
+        System.out.println("Frase 1 e Frase 2 possuen o mesmo conteudo? R: " + (frasel.equals(frase2)));
+    }
+}
